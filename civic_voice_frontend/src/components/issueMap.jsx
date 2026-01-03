546b36b370e4bdb8a6d554a92bbad557e27e950d
prashanth <prashanthannaldas453@gmail.com>
2026-01-04 02:11:07 +0530

    Prashanth Updated

---
+++ civic_voice_frontend/src/components/issueMap.jsx
@@ -0,0 +1,37 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix marker icons
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
+function IssuesMap({ issues }) {
+  return (
+    <MapContainer
+      center={[17.385, 78.486]}
+      zoom={13}
+      style={{ height: "450px", width: "100%" }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+      {issues.map((issue) => (
+        <Marker key={issue.id} position={[issue.lat, issue.lng]}>
+          <Popup>
+            <b>{issue.type}</b><br />
+            {issue.description}
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
+
+export default IssuesMap;
