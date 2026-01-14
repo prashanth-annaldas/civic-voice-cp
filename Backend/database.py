f9ed1fcf93fb5dbb4e0c0ff912b86b0a0b5b4301
prashanth-annaldas <prashanthannaldas453@gmail.com>
2026-01-15 00:47:52 +0530

    Update DATABASE_URL to new file path

--- Backend/database.py
+++ Backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./civicvoice.db"
+DATABASE_URL = "sqlite:////opt/render/project/src/civicvoice.db"
 
 engine = create_engine(
     DATABASE_URL,
